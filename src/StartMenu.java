c928e7f40cf681cdac9cc8e61efd35a08b869216
Roman Sattarov <enomine_90@mail.ru>
2022-05-10 22:38:02 +0500

    Full fixed method of encrypt and condition pathOfText, keyOfEncrypt and case1 Of StartMenu.

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,3 +1,5 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class StartMenu {
@@ -9,16 +11,38 @@ public class StartMenu {
         System.out.println("2. Дешифровка");
         System.out.println("3. Криптоанализ (brute force)");
         System.out.println("4. Выход)");
+        System.out.println("Введите число:");
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextInt()) {
                 switch (scanner.nextInt()) {
                     case 1:
-                        Scanner scanner11 = new Scanner(System.in);
-                        System.out.println("Введите путь к файлу:");
-                        String pathOfText = scanner11.next();
-                        System.out.println("Введите ключ:");
-                        Scanner scanner12 = new Scanner(System.in);
-                        int keyOfEncrypt = scanner12.nextInt();
+                        boolean condition1 = true;
+                        String pathOfText = null;
+                        boolean condition2 = true;
+                        int keyOfEncrypt = -1;
+                        // обработка адреса файла с текстом. (правильный ли адрес и существует ли файл)
+                        while (condition1) {
+                            System.out.println("Введите путь к файлу:");
+                            Scanner scanner11 = new Scanner(System.in);
+                            pathOfText = scanner11.nextLine();
+                            if (Path.of(pathOfText).isAbsolute() && Files.exists(Path.of(pathOfText))) {
+                                condition1 = false;
+                            } else {
+                                System.out.println("Путь к файлу неверный либо такого файла не существует!");
+                            }
+                        }
+                        // условие обработки вводимого ключа (интовый ключ в диапазоне от 0 до макс. инт)
+                        while (condition2) {
+                            System.out.println("Введите ключ:");
+                            Scanner scanner12 = new Scanner(System.in);
+                            keyOfEncrypt = scanner12.nextInt();
+                            if (keyOfEncrypt > 0 && keyOfEncrypt < Integer.MAX_VALUE) {
+                                condition2 = false;
+                            } else {
+                                System.out.println("Данное значение не подходит для ключа!");
+                            }
+                        }
+                        // Обращение к методу шифровальщика
                         Cryptographer.encrypt(pathOfText, keyOfEncrypt);
                         break;
                     case 2:
@@ -30,8 +54,12 @@ public class StartMenu {
                     case 4:
                         value = false;
                         break;
+                    default:
+                        System.out.println("Необходимо ввести число от 1 до 4");
                 }
-            }
+            } else {
+            System.out.println("Необходимо ввести число от 1 до 4");
+        }
         }
     }
 }
